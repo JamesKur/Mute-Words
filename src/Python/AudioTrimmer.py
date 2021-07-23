fc527b474930d9fbc8de813db3e1c27f41003051
JamesKur <jamesk543@gmail.com>
2021-07-23 02:10:41 -0500

    Integrating google cloud into speech recognition

--- src/Python/AudioTrimmer.py
+++ src/Python/AudioTrimmer.py
@@ -1,1 +1,6 @@
-print('hi')
+from pydub import AudioSegment 
+import ChannelTools
+
+#audio = ChannelTools.extractCenter("E:\Cache\gMKVExtract\Pineapple Express.thd")
+audio = AudioSegment.from_file("E:\Cache\gMKVExtract\Pineapple Expressfront_center.flac")
+print(audio.frame_rate)
